8b20bcad0fb7bc2c7ac0fae23e7759098e7805ad
Sahir Shaikh <shaikh.sahir50@gmail.com>
2024-04-19 15:27:12 +0530

    [Task] - Death After falling from the platform and going outside the screen downwards.

--- Assets/PlayerFall_Gameover.cs
+++ Assets/PlayerFall_Gameover.cs
@@ -2,13 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerFall_Gameover : MonoBehaviour
 {
+
+    public Animator animator;
     private void OnTriggerEnter2D(Collider2D other) {
-        // if(other.gameObject.GetComponent<PlayerController>() != null)
-        if(other.gameObject.CompareTag("Player"))
+        if(other.gameObject.GetComponent<PlayerController>() != null)
+
         {
             Debug.Log("Player Fall... Game Over...!!");
+            animator.SetTrigger("Death");
+
         }   
     }
 }
